60b0757fa3053c0254f7e4868bafecfe372592ea
billp <billp>
2014-02-13 17:45:38 +0000

    parallel: rework communicator so it has a light weight copy constructor (all reduce no longer required)

--- src/parallel/communicator.hpp
+++ src/parallel/communicator.hpp
@@ -8,7 +8,7 @@
 /**
  * @file   communicator.hpp
  * @author William A. Perkins
- * @date   2014-02-12 10:31:21 d3g096
+ * @date   2014-02-13 09:25:03 d3g096
  * 
  * @brief  
  * 
@@ -20,11 +20,14 @@
 #ifndef _communicator_hpp_
 #define _communicator_hpp_
 
-#include <boost/mpi/communicator.hpp>
-#include <ga.h>
+#include <boost/mpi.hpp>
+#include <boost/shared_ptr.hpp>
 
 namespace gridpack {
 namespace parallel {
+
+// forward declaration
+class CommunicatorPrivate;
 
 // -------------------------------------------------------------
 //  class Communicator
@@ -33,245 +36,106 @@ class Communicator {
 public:
 
   /// Default constructor (world)
-  Communicator(void)
-    : pComm()
-  {
-    p_GAgroup = GA_Pgroup_get_world();
-  }
+  Communicator(void);
 
   /// Copy constructor.
-  Communicator(const Communicator& old)
-    : pComm(old.pComm)
-  {
-    // Make a copy of GA process group
-    int grp = old.p_GAgroup;
-    if (grp != GA_Pgroup_get_world()) {
-      int me = GA_Pgroup_nodeid(grp); 
-      int nprocs = GA_Pgroup_nnodes(grp); 
-      int list[nprocs];
-      int i;
-      // We are just copying the group, so all processes show up
-      for (i=0; i<nprocs; i++) {
-        list[i] = i;
-      }
-      int defGrp = GA_Pgroup_get_default();
-      GA_Pgroup_set_default(grp);
-      p_GAgroup = GA_Pgroup_create(list,nprocs);
-      GA_Pgroup_set_default(defGrp);
-    } else {
-      p_GAgroup = grp;
-    }
-  }
+  Communicator(const Communicator& old);
 
   /// Construct with a Boost communicator
-  /// This will only work if all processes on the world communicator
-  /// call this function
-  Communicator(const boost::mpi::communicator& comm)
-    : pComm(comm)
-  {
-    int i, ierr, me, nprocs;
-    me = comm.rank();
-    nprocs = comm.size();
-    int gaWrld = GA_Pgroup_get_world();
-    int defGrp = GA_Pgroup_get_default();
-    GA_Pgroup_set_default(gaWrld);
-    // Construct a list of procs in the world group
-    int gaSrc[nprocs];
-    int gaDest[nprocs];
-    for (i=0; i<nprocs; i++) {
-      gaSrc[i] = 0;
-      gaDest[i] = 0;
-    }
-    gaSrc[me] = GA_Nodeid();
-    ierr = MPI_Allreduce(gaSrc,gaDest,nprocs,MPI_INT,MPI_SUM,comm);
-    p_GAgroup = GA_Pgroup_create(gaDest,nprocs);
-    GA_Pgroup_sync(p_GAgroup);
-    GA_Pgroup_set_default(defGrp);
-  }
+  /** 
+   * This will only work if all processes on the world communicator
+   * call this function (really !?!)
+   * 
+   * @param comm existing Communicator
+   * 
+   */
+  Communicator(const boost::mpi::communicator& comm);
 
-  Communicator(const MPI_Comm& comm)
-    : pComm(comm, boost::mpi::comm_duplicate)
-  {
-    int i, ierr, me, nprocs;
-    ierr = MPI_Comm_rank(comm, &me);
-    ierr = MPI_Comm_size(comm, &nprocs);
-    int gaWrld = GA_Pgroup_get_world();
-    int defGrp = GA_Pgroup_get_default();
-    GA_Pgroup_set_default(gaWrld);
-    // Construct a list of procs in the world group
-    int gaSrc[nprocs];
-    int gaDest[nprocs];
-    for (i=0; i<nprocs; i++) {
-      gaSrc[i] = 0;
-      gaDest[i] = 0;
-    }
-    gaSrc[me] = GA_Nodeid();
-    ierr = MPI_Allreduce(gaSrc,gaDest,nprocs,MPI_INT,MPI_SUM,comm);
-    p_GAgroup = GA_Pgroup_create(gaDest,nprocs);
-    GA_Pgroup_sync(p_GAgroup);
-    GA_Pgroup_set_default(defGrp);
-  }
+  /// Construct from an existing @c MPI_comm
+  /** 
+   * 
+   * 
+   * @param comm 
+   */
+  Communicator(const MPI_Comm& comm);
 
-  Communicator & operator=(const Communicator & rhs) {
-    if (this == &rhs) return *this;
-    pComm = rhs.pComm;
-    // Make a copy of processor group
-    int grp = rhs.p_GAgroup;
-    if (grp != GA_Pgroup_get_world()) {
-      int me = GA_Pgroup_nodeid(grp); 
-      int nprocs = GA_Pgroup_nnodes(grp); 
-      int list[nprocs];
-      int i;
-      // We are just copying the group, so all processes show up
-      for (i=0; i<nprocs; i++) {
-        list[i] = i;
-      }
-      int defGrp = GA_Pgroup_get_default();
-      GA_Pgroup_set_default(grp);
-      p_GAgroup = GA_Pgroup_create(list,nprocs);
-      GA_Pgroup_set_default(defGrp);
-    } else {
-      p_GAgroup = grp;
-    }
-  }
+  /// Make this instance the same as another
+  Communicator & operator=(const Communicator & rhs);
 
   /// Destructor
-  ~Communicator(void)
-  {
-    if (p_GAgroup != GA_Pgroup_get_world()){
-      GA_Pgroup_destroy(p_GAgroup);
-    }
-  }
+  ~Communicator(void);
 
   /// Get the size of this communicator
   int size(void) const
   {
-    return pComm.size();
+    return p_comm.size();
   }
 
   /// Get this process's rank in this communicator
   int rank(void) const
   {
-    return pComm.rank();
+    return p_comm.rank();
   }
 
   /// Get this process's rank in the world communicator
-  int worldRank(void) const
-  {
-    return GA_Nodeid();
-  }
+  int worldRank(void) const;
 
   /// cast to MPI communicator
   operator MPI_Comm() const
   {
-    return static_cast<MPI_Comm>(pComm);
+    return static_cast<MPI_Comm>(p_comm);
   }
 
   /// cast to boost communicator
   operator boost::mpi::communicator() const
   {
-    return pComm;
+    return p_comm;
   }
 
   void barrier() const
   {
-    pComm.barrier();
+    p_comm.barrier();
   }
 
   /// Split this instance into several communicators
   Communicator split(int color) const
   {
-    // start by creating process groups
-    int i, ierr, me, nprocs;
-    ierr = MPI_Comm_rank(pComm, &me);
-    ierr = MPI_Comm_size(pComm, &nprocs);
-    int defGrp = GA_Pgroup_get_default();
-    GA_Pgroup_set_default(p_GAgroup);
-    // Construct a list of procs in the world group
-    int gaColor[nprocs];
-    for (i=0; i<nprocs; i++) {
-      gaColor[i] = 0;
-    }
-    gaColor[me] = color;
-    GA_Pgroup_igop(p_GAgroup,gaColor,nprocs,"+");
-    int ncolor = 0;
-    for (i=0; i<nprocs; i++) {
-      if (gaColor[i] == color) ncolor++;
-    }
-    int group[ncolor];
-    ncolor = 0;
-    for (i=0; i<nprocs; i++) {
-      if (gaColor[i] == color) {
-        group[ncolor] = i;
-        ncolor++;
-      }
-    }
-    int GAgroup = GA_Pgroup_create(group,ncolor);
-    GA_Pgroup_set_default(defGrp);
-    GA_Sync();
-    Communicator ret;
-    ret.pComm = pComm.split(color);
-    ret.p_GAgroup = GAgroup;
-    return ret;
+    return Communicator(p_comm.split(color));
   }
 
-  // split this instance in several communicators, each of which contains at
-  // most nsize processors.
-  Communicator divide(int nsize) const
-  {
-    int nprocs = pComm.size();
-    int me = pComm.rank();
-    // find out how many communicators need to be created
-    int ngrp = nprocs/nsize;
-    if (ngrp*nsize < nprocs) ngrp++;
-    // evaluate how many communicators are of size nsize
-    int nlarge = nprocs;
-    if (ngrp*nsize > nprocs) {
-      int nsmall = ngrp*nsize - nprocs;
-      nlarge = nprocs - nsmall;
-    }
-    nlarge = nlarge/nsize;
-    if (nprocs - nlarge*nsize  > nsize) nlarge++;
-    // figure out what color this process is and use split functionality to
-    // create new communicators
-    int i;
-    int color = 0;
-    for (i=0; i<=me; i++) {
-      if (i<nlarge*nsize) {
-        if (i%nsize == 0) color++;
-      } else {
-        if ((i-nlarge*nsize)%(nsize-1) == 0) color++;
-      }
-    }
-    return this->split(color);
-  }
+  /// Split this instance in several communicators
+  /** 
+   * Each communicator created contains at most @c nsize processes
+   * 
+   * @param nsize 
+   * 
+   * @return 
+   */
+  Communicator divide(int nsize) const;
 
+  /// Get the MPI communicator for this instance (const version)
   const boost::mpi::communicator& getCommunicator(void) const
   {
-    return pComm;
+    return p_comm;
   }
 
-  boost::mpi::communicator& getCommunicator(void)
-  {
-    return pComm;
-  }
+  /// Get a (C-style) handle to for the GA process group described by this instance
+  int getGroup(void) const;
 
-  int getGroup(void) const
-  {
-    return p_GAgroup;
-  }
 
   /// Sync GA process group
-  void sync(void) const
-  {
-    GA_Pgroup_sync(p_GAgroup);
-  }    
+  void sync(void) const;
 
 protected:
   
+  /// Swap contents with another instance
+  void swap(Communicator& other);
+
   /// The Boost communicator 
-  boost::mpi::communicator pComm;
-  int p_GAgroup;
+  boost::mpi::communicator p_comm;
+
+  /// The private (GA process group) stuff
+  boost::shared_ptr<CommunicatorPrivate> p_private;
 };
 
 
