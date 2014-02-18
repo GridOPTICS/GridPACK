90df8efedcfede3c05cffb4e4e57468552ca1849
bruce <bruce>
2014-02-18 20:06:40 +0000

    Attempt to modify transpose operation so that it is more robust.

--- src/math/petsc/petsc_matrix_operations.cpp
+++ src/math/petsc/petsc_matrix_operations.cpp
@@ -19,6 +19,7 @@
 
 static const char* SCCS_ID = "$Id$ Battelle PNL";
 
+#include <ga.h>
 #include <boost/assert.hpp>
 #include <boost/format.hpp>
 #include "matrix.hpp"
@@ -71,7 +72,97 @@ transpose(const Matrix& A)
   
   PetscErrorCode ierr(0);
   try {
-    ierr = MatTranspose(*pA, MAT_INITIAL_MATRIX, &pAtrans); CHKERRXX(ierr);
+    MatType type;
+    MatGetType(*pA,&type);
+    if (strcmp(type,MATMPIDENSE) == 0 && A.communicator().size() > 1) {
+      // This is a hack to get around problems with changing distributions in
+      // PETSc. Copy the matrix to a global array, do the transpose in GA, and
+      // then copy to a new PETSc matrix while preserving the distribution of the
+      // original PETSc matrix.
+      // Get dimensions of matrix A and allocate local arrays
+      int lrows(A.localRows());
+      int grows(A.rows());
+      int gcols(A.cols());
+      ComplexType vals[lrows*gcols];
+      int nprocs = A.communicator().size();
+      int me = A.communicator().rank();
+      int tmapc[nprocs+1];
+      int mapc[nprocs+1];
+      //printf("p[%d] (transpose) Got to 1\n",me);
+      // Set up global arrays with same row partition as A
+      int i, j;
+      for (i=0; i<nprocs+1; i++) tmapc[i] = 0;
+      tmapc[me] = lrows;
+      GA_Pgroup_igop(A.communicator().getGroup(),tmapc,nprocs+1,"+");
+      mapc[0] = 0;
+      for (i=1; i<nprocs; i++) mapc[i] = mapc[i-1]+tmapc[i-1];
+      mapc[nprocs] = 0;
+      int dims[2],blocks[2];
+      dims[0] = grows;
+      dims[1] = gcols;
+      blocks[0] = nprocs;
+      blocks[1] = 1;
+      //printf("p[%d] (transpose) Got to 2\n",me);
+      int g_tmp = GA_Create_handle();
+      GA_Set_data(g_tmp,2,dims,C_DCPL);
+      GA_Set_irreg_distr(g_tmp,mapc,blocks);
+      GA_Set_pgroup(g_tmp,A.communicator().getGroup());
+      if (!GA_Allocate(g_tmp)) {
+        //TODO: some kind of error
+      }
+      int g_trns = GA_Create_handle();
+      GA_Set_data(g_trns,2,dims,C_DCPL);
+      GA_Set_irreg_distr(g_trns,mapc,blocks);
+      GA_Set_pgroup(g_trns,A.communicator().getGroup());
+      if (!GA_Allocate(g_trns)) {
+        //TODO: some kind of error
+      }
+      //printf("p[%d] (transpose) Got to 3\n",me);
+      // Get low and high row indices for matrix A and copy local row block of A
+      // to g_tmp
+      int llo,lhi, ncount;
+      A.localRowRange(llo,lhi);
+      ncount = 0;
+      for (j=0; j<gcols; j++) {
+        for (i=llo; i<lhi; i++) {
+          A.getElement(i,j,vals[ncount]);
+          ncount++;
+        }
+      }
+      int lo[2],hi[2];
+      //printf("p[%d] (transpose) Got to 4\n",me);
+      // Transpose g_tmp
+      NGA_Put(g_tmp,lo,hi,vals,&gcols);
+      GA_Transpose(g_tmp, g_trns);
+      NGA_Get(g_trns,lo,hi,vals,&gcols);
+      // Copy g_trns to new PETSc matrix
+      GA_Pgroup_sync(A.communicator().getGroup());
+      //printf("p[%d] (transpose) Got to 5\n",me);
+      ierr = MatCreate(A.communicator(), &pAtrans); CHKERRXX(ierr);
+      ierr = MatSetSizes(pAtrans, lrows,lrows,PETSC_DETERMINE,PETSC_DETERMINE); CHKERRXX(ierr);
+      //printf("p[%d] (transpose) Got to 5a\n",me);
+      ierr = MatSetType(pAtrans, type); CHKERRXX(ierr);
+      //printf("p[%d] (transpose) Got to 5b\n",me);
+      ierr = MatMPIDenseSetPreallocation(pAtrans, PETSC_NULL); CHKERRXX(ierr);
+      //printf("p[%d] (transpose) Got to 5c\n",me);
+      ncount = 0;
+      for (j=0; j<gcols; j++) {
+        for (i=llo; i<lhi; i++) {
+          ierr = MatSetValue(pAtrans,i,j,vals[ncount],INSERT_VALUES); CHKERRXX(ierr);
+          ncount++;
+        }
+      }
+      //printf("p[%d] (transpose) Got to 5d\n",me);
+      ierr = MatAssemblyBegin(pAtrans, MAT_FINAL_ASSEMBLY); CHKERRXX(ierr);
+      ierr = MatAssemblyEnd(pAtrans, MAT_FINAL_ASSEMBLY); CHKERRXX(ierr);
+      //printf("p[%d] (transpose) Got to 6\n",me);
+      GA_Destroy(g_tmp);
+      GA_Destroy(g_trns);
+      GA_Pgroup_sync(A.communicator().getGroup());
+      //printf("p[%d] (transpose) Got to 7\n",me);
+    } else {
+      ierr = MatTranspose(*pA, MAT_INITIAL_MATRIX, &pAtrans); CHKERRXX(ierr);
+    }
   } catch (const PETSc::Exception& e) {
     throw PETScException(ierr, e);
   }
@@ -182,6 +273,15 @@ multiply(const Matrix& A, const Vector& x, Vector& result)
 
   PetscErrorCode ierr(0);
   try {
+    PetscInt plo, phi;
+    MatType type;
+    ierr = MatGetType(*Amat,&type);
+    ierr = MatGetOwnershipRange(*Amat,&plo,&phi);
+    ierr = MatGetSize(*Amat,&plo,&phi);
+    ierr = VecGetOwnershipRange(*Xvec,&plo,&phi);
+    ierr = VecGetSize(*Xvec,&plo);
+    ierr = VecGetOwnershipRange(*Yvec,&plo,&phi);
+    ierr = VecGetSize(*Yvec,&plo);
     ierr = MatMult(*Amat, *Xvec, *Yvec); CHKERRXX(ierr);
   } catch (const PETSc::Exception& e) {
     throw PETScException(ierr, e);
