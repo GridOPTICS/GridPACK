c08f5b67df366cc74d3b67402c2cc53c1dbd9129
William Perkins <william.perkins@pnnl.gov>
2015-01-20 10:01:58 -0800

    construct and test a compilation-time test for proper numeric types

---
+++ src/math/numeric_type_check.hpp
@@ -0,0 +1,68 @@
+// Emacs Mode Line: -*- Mode:c++;-*-
+// -------------------------------------------------------------
+/*
+ *     Copyright (c) 2013 Battelle Memorial Institute
+ *     Licensed under modified BSD License. A copy of this license can be found
+ *     in the LICENSE file in the top level directory of this distribution.
+ */
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+/**
+ * @file   numeric_type_check.hpp
+ * @author William A. Perkins
+ * @date   2015-01-20 09:34:49 d3g096
+ * 
+ * @brief  
+ * 
+ * 
+ */
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+// Created January 20, 2015 by William A. Perkins
+// Last Change: 2013-05-03 12:23:12 d3g096
+// -------------------------------------------------------------
+
+
+#ifndef _numeric_type_check_hpp_
+#define _numeric_type_check_hpp_
+
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/find.hpp>
+#include <boost/type_traits/is_same.hpp>
+
+#include <gridpack/utilities/complex.hpp>
+
+namespace gridpack {
+namespace math {
+
+/// This is used to make sure the numeric type is supported
+/**
+ * GridPACK only supports a limited number of element types for
+ * matrices and vectors, specifically @c double and @c
+ * "std::complex<double>. This will cause a compilation failure if the
+ * type is not supported.
+ * 
+ */
+template <typename T>
+struct TypeCheck {
+private:
+  typedef boost::mpl::vector<RealType, ComplexType> Allowable;
+  typedef boost::mpl::end<Allowable>::type NotFound;
+  typedef typename boost::mpl::find<Allowable, T>::type Found;
+public:
+  typedef boost::mpl::not_<
+    boost::is_same<Found, NotFound>
+  > OK;
+  static bool check(void) 
+  {
+    return OK::value;
+  }
+};
+
+} // namespace math
+} // namespace gridpack
+
+
+
+
+#endif
---
+++ src/math/test/numeric_test.cpp
@@ -0,0 +1,64 @@
+// -------------------------------------------------------------
+/*
+ *     Copyright (c) 2013 Battelle Memorial Institute
+ *     Licensed under modified BSD License. A copy of this license can be found
+ *     in the LICENSE file in the top level directory of this distribution.
+ */
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+/**
+ * @file   numeric_test.cpp
+ * @author William A. Perkins
+ * @date   2015-01-20 09:51:53 d3g096
+ * 
+ * @brief  
+ * 
+ * 
+ */
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+// Created January 20, 2015 by William A. Perkins
+// Last Change: 2013-05-03 12:23:12 d3g096
+// -------------------------------------------------------------
+
+#include <iostream>
+
+#include "numeric_type_check.hpp"
+
+
+#define BOOST_TEST_NO_MAIN
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(Numeric)
+
+BOOST_AUTO_TEST_CASE(TypeCheck)
+{
+  BOOST_CHECK(gridpack::math::TypeCheck<gridpack::RealType>::check());
+  BOOST_CHECK(gridpack::math::TypeCheck<gridpack::ComplexType>::check());
+  BOOST_CHECK(!gridpack::math::TypeCheck<float>::check());
+  BOOST_CHECK(!gridpack::math::TypeCheck<int>::check());
+  BOOST_CHECK(gridpack::math::TypeCheck<double>::check());
+  BOOST_CHECK(gridpack::math::TypeCheck< std::complex<double> >::check()); 
+  BOOST_CHECK(!gridpack::math::TypeCheck< std::complex<float> >::check()); 
+}
+  
+BOOST_AUTO_TEST_SUITE_END()
+
+// -------------------------------------------------------------
+// init_function
+// -------------------------------------------------------------
+bool init_function()
+{
+  return true;
+}
+
+// -------------------------------------------------------------
+//  Main Program
+// -------------------------------------------------------------
+int
+main(int argc, char **argv)
+{
+  int result = ::boost::unit_test::unit_test_main( &init_function, argc, argv );
+  return result;
+}
