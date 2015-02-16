75ca17dd756ea7f7d16a7af4c575e1eb8e5f1b3e
Bruce J Palmer <d3g293@olympus.local>
2015-02-16 10:36:51 -0800

    Adding test routine for the powerflow module to repository.

---
+++ src/applications/modules/powerflow/test/pf_test.cpp
@@ -0,0 +1,67 @@
+/*
+ *     Copyright (c) 2013 Battelle Memorial Institute
+ *     Licensed under modified BSD License. A copy of this license can be found
+ *     in the LICENSE file in the top level directory of this distribution.
+ */
+// -------------------------------------------------------------
+/**
+ * @file   rtpr_main.cpp
+ * @author Bruce Palmer
+ * @date   January 15, 2015
+ *
+ * @brief
+ */
+// -------------------------------------------------------------
+
+#include "mpi.h"
+#include <ga.h>
+#include <macdecls.h>
+#include "gridpack/math/math.hpp"
+#include "pf_app_module.hpp"
+
+// Calling program for the contingency_analysis applications
+
+main(int argc, char **argv)
+{
+  // Initialize MPI libraries
+  int ierr = MPI_Init(&argc, &argv);
+  // Intialize Math libraries
+  gridpack::math::Initialize();
+
+  GA_Initialize();
+  int stack = 200000, heap = 200000;
+  MA_init(C_DBL, stack, heap);
+
+  if (1) {
+    gridpack::parallel::Communicator world;
+
+    // read configuration file
+    gridpack::utility::Configuration *config =
+      gridpack::utility::Configuration::configuration();
+    if (argc >= 2 && argv[1] != NULL) {
+      char inputfile[256];
+      sprintf(inputfile,"%s",argv[1]);
+      config->open(inputfile,world);
+    } else {
+      config->open("input.xml",world);
+    }
+
+    // setup and run powerflow calculation
+    boost::shared_ptr<gridpack::powerflow::PFNetwork>
+      pf_network(new gridpack::powerflow::PFNetwork(world));
+
+    gridpack::powerflow::PFAppModule pf_app;
+    pf_app.readNetwork(pf_network,config);
+    pf_app.initialize();
+    pf_app.solve();
+    pf_app.write();
+  }
+
+  GA_Terminate();
+
+  // Terminate Math libraries
+  gridpack::math::Finalize();
+  // Clean up MPI libraries
+  ierr = MPI_Finalize();
+}
+
