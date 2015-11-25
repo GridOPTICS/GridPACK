edd8cba3563ebc3f37a7b9952c11f1490987d381
Yilin Fang <d3m045@jiaolong.pnl.gov>
2015-11-25 14:13:08 -0800

    Correct global constraint implementation.

--- src/optimization/optimization.hpp
+++ src/optimization/optimization.hpp
@@ -25,13 +25,8 @@
 #include "gridpack/network/base_network.hpp"
 #include "gridpack/component/base_component.hpp"
 #include "gridpack/component/optimization_ifc.hpp"
-//#include "gridpack/expression/variable.hpp"
-//#include "gridpack/expression/expression.hpp"
-//#include "gridpack/expression/optimizer.hpp"
 #include <boost/smart_ptr/shared_ptr.hpp>
 
-//#include "
-//#include <gridpack/parallel/distributed.hpp>
 #include <ga.h>
 
 
@@ -97,12 +92,6 @@ class NetworkOptimizer
       totalGen = 0;
     }
 
-//    Optimizer(const gridpack::parallel::Communicator& comm, NetworkPtr network)
-//      : p_network(network),gridpack::parallel::Communicator()
-//    { 
-//      p_nBuses = p_network->numBuses();
-//      totalGen = 0;
-//    }
 
     /**
      * Destructor
@@ -126,12 +115,6 @@ class NetworkOptimizer
       int grp = p_network->communicator().getGroup();
       int nprocs = GA_Pgroup_nnodes(grp);
       int me = GA_Pgroup_nodeid(grp);
-//      for (int i=0; i<p_nBuses; i++){
-//        if(p_network->getActiveBus(i)) {
-//          ngen = p_network->getBus(i)->numGen;
-//          totalGen += ngen;
-//        }
-//      }
       int loc_totalGen = totalGen;
 
       int genArr[nprocs];
@@ -146,64 +129,63 @@ class NetworkOptimizer
       for (int p=1; p<nprocs; p++) {
         offset[p]= offset[p-1] + genArr[p-1];
       }
-//printf("unit and hori--%d %d\n",p_numHorizons,p_numUnits);
 // 
-       int inc = offset[me];
-//printf("me--%d %d\n",me,inc);
+//       int inc = offset[me];
+       int inc = 0; 
 // power produced
        for (int p = 0; p < p_numHorizons; p++) {
          for (int i = 0; i < p_numUnits; i++) {
            rval = uc_maxPower[i];
            VarPtr vptr (new RealVariable(0.0, 0.0, rval));
-           vptr->name(boost::str(boost::format("id%d") % inc));
+           vptr->name(boost::str(boost::format("p_u_%d_t_%d")  %i %p));
+
            ret.push_back(vptr);
-//           std::cout << vptr->name() << " id is " << vptr->id() << std::endl;
            inc++;
          }
        }
 
 // reserve required
+       inc = 0; 
        for (int p = 0; p < p_numHorizons; p++) {
          for (int i = 0; i < p_numUnits; i++) {
            rval = uc_maxPower[i];
            VarPtr vptr (new RealVariable(0.0, 0.0, rval));
-           vptr->name(boost::str(boost::format("id%d") % inc));
+           vptr->name(boost::str(boost::format("r_u_%d_t_%d") %i %p));
            ret.push_back(vptr);
 
-//           std::cout << vptr->name() << " id is " << vptr->id() << std::endl;
            inc++;
          }
        }
 // onoff 
+       inc = 0; 
        for (int p = 0; p < p_numHorizons; p++) {
          for (int i = 0; i < p_numUnits; i++) {
            VarPtr vptr (new IntegerVariable(0,0,1));
-           vptr->name(boost::str(boost::format("id%d") % inc));
+           vptr->name(boost::str(boost::format("on_u_%d_t_%d") %i %p));
            ret.push_back(vptr);
 
-//           std::cout << vptr->name() << " id is " << vptr->id() << std::endl;
            inc++;
          }
        }
 // start up 
+       inc = 0; 
        for (int p = 0; p < p_numHorizons; p++) {
          for (int i = 0; i < p_numUnits; i++) {
            VarPtr vptr (new IntegerVariable(0,0,1));
-           vptr->name(boost::str(boost::format("id%d") % inc));
+           vptr->name(boost::str(boost::format("up_u_%d_t_%d") %i %p));
            ret.push_back(vptr);
 
-//           std::cout << vptr->name() << " id is " << vptr->id() << std::endl;
            inc++;
          }
        }
-// shut down 
+// shut down
+       inc = 0; 
        for (int p = 0; p < p_numHorizons; p++) {
          for (int i = 0; i < p_numUnits; i++) {
            VarPtr vptr (new IntegerVariable(0,0,1));
-           vptr->name(boost::str(boost::format("id%d") % inc));
+           vptr->name(boost::str(boost::format("dn_u_%d_t_%d") %i %p));
            ret.push_back(vptr);
 
-//           std::cout << vptr->name() << " id is " << vptr->id() << std::endl;
            inc++;
          }
        }
@@ -218,11 +200,52 @@ class NetworkOptimizer
      * null pointer
      */
 //    boost::shared_ptr<gridpack::optimization::Expression>
-    ExpPtr
+    std::vector<ExpPtr>
       getGlobalConstraint(const char* tag)
     {
-//      boost::shared_ptr<gridpack::optimization::Expression> ret;
-      ExpPtr ret;
+      std::vector<VarPtr> vlist;
+      vlist.clear();
+      vlist = this->getVariables();
+      int nVar = p_numHorizons*p_numUnits;
+      int nVarP = nVar;
+      VarPtr powerProduced;
+      VarPtr powerReserved;
+//  Initial state, treat as constraint
+      int powerCnt;
+      int reserveCnt;
+// get the list of variables; clear() is required, otherwise, variable number is wrong
+      std::vector<ExpPtr> ret;
+      ret.clear();
+
+// energy balance
+      for (int p = 1; p < p_numHorizons; p++) {
+        ExpPtr exprgP;
+        exprgP.reset();
+        ExpPtr exprgR;
+        exprgR.reset();
+
+        for (int i = 0; i < p_numUnits; i++) {
+           powerCnt = p*p_numUnits + i;
+           powerProduced = vlist[powerCnt];
+           reserveCnt = nVarP + powerCnt;
+           powerReserved = vlist[reserveCnt];
+           if(!exprgP) {
+             exprgP = 1*powerProduced;
+           }else{
+             exprgP = exprgP + 1*powerProduced;
+           }
+
+           if(!exprgR) {
+             exprgR = 1*powerReserved;
+           }else{
+             exprgR = exprgR + 1*powerReserved;
+           }
+        }
+        ret.push_back(exprgP);
+
+        ret.push_back(exprgR);
+
+      }
       return ret;
     }
 
@@ -234,19 +257,10 @@ class NetworkOptimizer
     std::vector<ConstPtr>
       getLocalConstraints()
     {
-//      std::vector<boost::shared_ptr<gridpack::optimization::Constraint> > ret;
-//      typedef boost::shared_ptr<gridpack::optimization::Constraint> ConstPtr;
       ConstPtr con;
-//      VarPtr vptr;
-//      vptr->clear(); 
       std::vector<VarPtr> vlist;
       vlist.clear();
       vlist = this->getVariables();
-//  for (std::vector<VarPtr>::iterator i = vlist.begin();
-//       i != vlist.end(); ++i) {
-//    std::cout <<" new " << (*i)->name() << std::endl;
-//}
-//      gridpack::optimization::ConstraintPtr con;
       ExpPtr expr;
       ExpPtr expr2;
       ExpPtr upDnIndicator;
@@ -279,34 +293,21 @@ class NetworkOptimizer
       for(int i=0; i< p_numUnits; i++) {
         onOffCnt = nVarR + i;
         con = vlist[onOffCnt] == 1;
-//        con->evaluate(); std::cout << std::endl;
         ret.push_back(con);
 
         start_UpCnt = nIntVcntOnoff + i;
         con = vlist[start_UpCnt] == 0;
-//        con->evaluate(); std::cout << std::endl;
         ret.push_back(con);
 
         shutDownCnt = nIntVcntStartup + i;
         con = vlist[shutDownCnt] == 0;
-//        con->evaluate(); std::cout << std::endl;
         ret.push_back(con);
 
         powerCnt = i;
         con = vlist[powerCnt] == uc_iniLevel[i];
-//        con->evaluate(); std::cout << std::endl;
         ret.push_back(con);
 
-/**
-      ucmdl.add(onOff[0][i] == 1);
-      ucmdl.add(start_Up[0][i] == 0);
-      ucmdl.add(shutDown[0][i] == 0);
-      ucmdl.add(powerProduced[0][i] == iniLevel[i]);
-**/
       }
-#if 0
-#endif
-//      vptr->clear(); 
       for (int p = 1; p < p_numHorizons; p++) {
         ExpPtr exprgP;
         exprgP.reset();
@@ -327,30 +328,22 @@ class NetworkOptimizer
 
            expr = powerProduced - 10000*onOff;
            con = expr <= 0;
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( expr1 <= 0);
 
            expr = powerProduced - uc_minPower[i]*onOff;
            con = expr >= 0;
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( expr2 >= 0);
 
 // ramp up constraint
            powerCntm1 = (p-1)*p_numUnits + i;
            expr = powerProduced + powerReserved - vlist[powerCntm1];
            con = expr <= uc_rampUp[i];
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( expr1 <= rampUp[i]);
 
 // ramp down constraint
            expr = vlist[powerCntm1]-powerProduced;
            con = expr <= uc_rampDown[i];
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( expr2 <= rampDown[i]);
 
 // minium up and down time
 // on at horizon p
@@ -365,9 +358,7 @@ class NetworkOptimizer
               onOffCnttmp = nVarR + j*p_numUnits + i;;
               onOfftmp = vlist[onOffCnttmp];
               con = upDnIndicator - 10000*onOfftmp <= 0;
-//              con->evaluate(); std::cout << std::endl;
               ret.push_back(con);
-//             ucmdl.add( upDnIndicator0 - 10000*onOff[j][i] <= 0);
             }
            } else{
              upDnPeriod = std::min(p_numHorizons, (p+(int)(uc_minUpTime[i]+0.5)));
@@ -375,18 +366,14 @@ class NetworkOptimizer
               onOffCnttmp = nVarR + j*p_numUnits + i;;
               onOfftmp = vlist[onOffCnttmp];
               con = upDnIndicator - 10000*onOfftmp <= 0;
-//              con->evaluate(); std::cout << std::endl;
               ret.push_back(con);
-//           ucmdl.add( upDnIndicator - 10000*onOff[j][i] <= 0);
             }
            }
 
 // start up, previous off
            upDnIndicator = onOff - vlist[onOffCntm1];
            con = upDnIndicator - 10000*start_Up <= 0;
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( upDnIndicator - 10000*start_Up[p][i] <= 0);
 
 // off at horizon p
            upDnIndicator = - 1*vlist[onOffCntm1] + 1*onOff;
@@ -395,17 +382,12 @@ class NetworkOptimizer
              onOffCnttmp = nVarR + j*p_numUnits + i;;
              onOfftmp = vlist[onOffCnttmp];
              con = upDnIndicator - 10000*onOfftmp <= -1;
-//             con->evaluate(); std::cout << std::endl;
              ret.push_back(con);
-//           ucmdl.add( upDnIndicator - 10000*onOff[j][i] <= 0);
            }
 // shut down, previous on
            upDnIndicator = vlist[onOffCntm1] - onOff;
-//         upDnIndicator = onOff[p-1][i] - onOff[p][i];
            con = upDnIndicator - 10000*shutDown <= 0;
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( upDnIndicator - 10000*shutDown[p][i] <= 0);
 
 // generation limits
 // startup at horizon p
@@ -413,9 +395,7 @@ class NetworkOptimizer
            - uc_maxPower[i]*onOff + uc_minPower[i]*onOff
                + uc_maxPower[i]*start_Up - uc_startCap[i]*start_Up;
            con = expr <= uc_minPower[i];
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( expr1 <= expr2);
 
 // shutdown at horizon p 
            onOffCntm1 = nVarP + powerCntm1;
@@ -424,57 +404,10 @@ class NetworkOptimizer
             uc_maxPower[i]*vlist[onOffCnttmp] + uc_minPower[i]*vlist[onOffCnttmp]
                 +uc_maxPower[i]*shutDown - uc_shutCap[i]*shutDown;
            con = expr  <= uc_minPower[i];
-//           con->evaluate(); std::cout << std::endl;
            ret.push_back(con);
-//         ucmdl.add( expr1 <= expr2);
-#if 0
-#endif
         }
 
-// global constraint
-
-        for (int i = 0; i < p_numUnits; i++) {
-           powerCnt = p*p_numUnits + i;
-           powerProduced = vlist[powerCnt];
-           reserveCnt = nVarP + powerCnt;
-           powerReserved = vlist[reserveCnt];
-           if(!exprgP) {
-             exprgP = 1*powerProduced;
-           }else{
-             exprgP = exprgP + 1*powerProduced;
-           }
-
-           if(!exprgR) {
-             exprgR = 1*powerReserved;
-           }else{
-             exprgR = exprgR + 1*powerReserved;
-           }
-        }
-//printf("after global---0%d %d\n",p_numUnits,p);
-        con->name("Ebalance");
-        con = exprgP == demand[p];
-//        con->evaluate(); std::cout << std::endl;
-        ret.push_back(con);
-
-//printf("after global---r%f\n",reserve[p]);
-        con->name("Rbalance");
-        con = exprgR >= reserve[p];
-//        con->evaluate(); std::cout << std::endl;
-        ret.push_back(con);
-//printf("after global---1\n");
-
-/**
-      expr3 = IloSum(powerProduced[p]);
-      ucmdl.add( expr3 == demand[p]);
-      expr3.end();
-      expr3 = IloSum(powerReserved[p]);
-      ucmdl.add( expr3 >= reserve[p]);
-      expr3.end();
-**/
-#if 0
-#endif
       }
-//printf("finish global---1\n");
       return ret;
     }
 
@@ -521,10 +454,6 @@ class NetworkOptimizer
          }
         }
       }
-      std::cout << std::endl;
-      std::cout << "The objective function is:" << std::endl;
-//      obj->evaluate(); std::cout << std::endl;
-//printf("finished obj ---\n");
       return obj;
     }
 
@@ -534,11 +463,6 @@ class NetworkOptimizer
      */
     double objectiveFunction(void)
     {
-//      gridpack::utility::CoarseTimer *timer =
-//        gridpack::utility::CoarseTimer::instance();
-//      timer->configTimer(p_profile);
-//      int t_setc = timer->createCategory("Factory:setComponents");
-//      timer->start(t_setc);
       int numBus = p_network->numBuses();
       int i, j;
       int idx1, idx2;
@@ -553,8 +477,6 @@ class NetworkOptimizer
       }
       GA_Dgop(&sum,1,"+");
       return sum;
-//      timer->stop(t_setc);
-//      timer->configTimer(true);
     }
     /**
       * solution
@@ -649,18 +571,6 @@ class NetworkOptimizer
           data->getValue("GENERATOR_QUAD_COST", &rvar, idx);
           costQuad.push_back(rvar);
         }
-/**
-  std::list<go::VariablePtr> vlist;
-  vlist.push_back(go::VariablePtr(new go::RealVariable(0.0, -1.0, 1.0)));
-  vlist.push_back(go::VariablePtr(new go::IntegerVariable(0, -1, 1)));
-  vlist.push_back(go::VariablePtr(new go::BinaryVariable(1)));
-  
-  VariablePrinter vp;
-  for (std::list<go::VariablePtr>::iterator i = vlist.begin();
-       i != vlist.end(); ++i) {
-    (*i)->accept(vp);
-  } 
-**/
       } 
     }
     /**
@@ -785,7 +695,6 @@ class NetworkOptimizer
           p_numUnits += ngen;
         }
       }
-//printf("loadsinfo- %d\n",p_numUnits);
       p_numHorizons = numHorizons;
 //
       demand.clear();
@@ -804,7 +713,6 @@ class NetworkOptimizer
     int  p_nBuses;
     int p_numUnits;
     int p_numHorizons;
-//    std::vector<VarPtr> ret;
 };
 
 }    // optimization
