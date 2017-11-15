a73200f3298fa09afe498c13b03d2b486c978ec7
Bruce J Palmer <d3g293@constance03.pnl.gov>
2017-11-15 09:56:55 -0800

    Got rid of variable length arrays in BaseParser and BusTable modules.

--- src/parser/bus_table.hpp
+++ src/parser/bus_table.hpp
@@ -306,19 +306,17 @@ public:
     }
     //construct array of indices to retrieve data
     int nsize = p_local_idx.size();
-    double v[nsize];
-    int array[nsize];
-    int **subscript;
-    subscript = new int*[nsize];
-    int *ptr = array;
+    std::vector<double> v(nsize);
+    std::vector<int> array(nsize);
+    std::vector<int*> subscript(nsize);
+    int *ptr = &array[0];
     int i;
     for (i=0; i<nsize; i++) {
       array[i] = p_nvals*p_order[i] + idx;
       subscript[i] = ptr;
       ptr++;
     }
-    NGA_Gather(p_GA_data, v, subscript, nsize);
-    delete [] subscript;
+    NGA_Gather(p_GA_data, &v[0], &subscript[0], nsize);
     p_comm.sync();
     for (i=0; i<nsize; i++) {
       values.push_back(v[i]);
