f85a077be26950a61c3e33fad6ea643016fc1ec5
William Perkins <william.perkins@pnnl.gov>
2019-08-16 14:23:34 -0700

    Give each (shared) library a version number.

--- src/cmake-modules/GridPACK.cmake
+++ src/cmake-modules/GridPACK.cmake
@@ -10,8 +10,10 @@
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # Created June 10, 2013 by William A. Perkins
-# Last Change: 2019-08-16 08:46:59 d3g096
+# Last Change: 2019-08-16 13:41:26 d3g096
 # -------------------------------------------------------------
+
+
 
 # This is used to specify a time out for GridPACK unit tests. It's 5
 # seconds by default, but may need to be longer on some platforms.
@@ -20,7 +22,10 @@ if (NOT GRIDPACK_TEST_TIMEOUT)
     CACHE STRING "Time out for GridPACK unit tests.")
 endif ()
 
+# -------------------------------------------------------------
+# set_tests_ldpath
 # Use a macro to set each test's environment
+# -------------------------------------------------------------
 macro(set_tests_ldpath tname)
   if(GRIDPACK_TEST_LDPATH)
     if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
@@ -32,6 +37,17 @@ macro(set_tests_ldpath tname)
         ENVIRONMENT LD_LIBRARY_PATH=${GRIDPACK_TEST_LDPATH}
         )
     endif()
+  endif()
+endmacro()
+
+# -------------------------------------------------------------
+# gridpack_set_library_version
+# -------------------------------------------------------------
+macro(gridpack_set_library_version lname)
+  if(BUILD_SHARED_LIBS)
+    set_target_properties(${lname}
+      PROPERTIES
+      SOVERSION "${GridPACK_VERSION_MAJOR}.${GridPACK_VERSION_MINOR}")
   endif()
 endmacro()
 
