57c2b55834d1dd9a3622bbb10a6d18b562347a5c
Bruce J Palmer <d3g293@constance03.pnl.gov>
2020-01-23 11:56:00 -0800

    Added check to make sure that all processors have the same number of timing variables before executing the dump function. This will prevent hangs if one or more processes have failed to create a timing variable.

--- src/timer/coarse_timer.cpp
+++ src/timer/coarse_timer.cpp
@@ -91,6 +91,16 @@ void gridpack::utility::CoarseTimer::dump(void) const
   double *rtime = new double[nproc];
 
   int size = p_title.size();
+  int size_min,size_max;
+  MPI_Allreduce(&size,&size_min,nproc,MPI_INT,MPI_MIN, world);
+  MPI_Allreduce(&size,&size_max,nproc,MPI_INT,MPI_MAX, world);
+  if (size_max != size_min) {
+    if (me == 0) {
+      printf ("Different numbers of timing catagories on\n");
+      printf ("different processors min: %d max: %d\n",size_min,size_max);
+      return;
+    }
+  }
   for (i = 0; i<size; i++) {
     // statistics over all processors
     for (j=0; j<nproc; j++) {
