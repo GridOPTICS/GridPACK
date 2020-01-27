675e9e219eba35417ba7bd4facdce32613b42732
William Perkins <william.perkins@pnnl.gov>
2020-01-27 09:13:48 -0800

    Add the Communicator class

--- src/gridpack.cpp
+++ src/gridpack.cpp
@@ -10,7 +10,7 @@
 // -------------------------------------------------------------
 // -------------------------------------------------------------
 // Created January 24, 2020 by Perkins
-// Last Change: 2020-01-24 13:01:58 d3g096
+// Last Change: 2020-01-27 08:04:49 d3g096
 // -------------------------------------------------------------
 
 #include <pybind11/pybind11.h>
@@ -21,6 +21,27 @@ namespace py = pybind11;
 namespace gp = gridpack;
 namespace gpp = gridpack::parallel;
 
+// GridPACK uses Boost smart pointers, so let's use those here
+PYBIND11_DECLARE_HOLDER_TYPE(T, boost::shared_ptr<T>, false);
+
 PYBIND11_MODULE(gridpack, gpm) {
   gpm.doc() = "GridPACK module";
+
+  // gridpack::Envronment class
+  py::class_<gp::Environment, boost::shared_ptr<gp::Environment> >(gpm, "Environment")
+  ;
+
+  // gridpack::parallel::Communicator class
+  py::class_<gpp::Communicator>(gpm, "Communicator")
+    .def("size", &gpp::Communicator::size)
+    .def("rank", &gpp::Communicator::rank)
+    .def("worldRank", &gpp::Communicator::worldRank)
+    .def("barrier", &gpp::Communicator::barrier)
+    .def("sync", &gpp::Communicator::sync)
+    .def("divide", &gpp::Communicator::divide)
+    .def("split", &gpp::Communicator::split)
+    ;
+    
+  // gridpack::parallel::TaskManager class
+  
 }
