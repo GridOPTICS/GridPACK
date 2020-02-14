24cf3597f4c496b9355e50d63a01b7b100891dfa
William Perkins <william.perkins@pnnl.gov>
2020-02-14 14:32:25 -0800

    Add a hack to deal with RHEL stupidity

--- src/sandbox/test_mpi/src/test_mpi_wrap.cpp
+++ src/sandbox/test_mpi/src/test_mpi_wrap.cpp
@@ -8,11 +8,9 @@
  */
 // -------------------------------------------------------------
 // Created February 14, 2020 by Perkins
-// Last Change: 2020-02-14 09:58:49 d3g096
+// Last Change: 2020-02-14 14:29:50 d3g096
 // -------------------------------------------------------------
 
-
-#include <vector>
 
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
@@ -20,7 +18,43 @@ namespace py = pybind11;
 
 #include "test_mpi.hpp"
 
+// Some temporary hacks
+
+#define RHEL_OPENMPI_HACK 0
+#ifdef RHEL_OPENMPI_HACK
+
+// This stupidity is needed on RHEL7 with stock OpenMPI packages
+// installed.
+
+#include <dlfcn.h>
+
+// -------------------------------------------------------------
+// stupid_openmpi_hack
+// from https://github.com/baidu-research/tensorflow-allreduce/issues/4
+// -------------------------------------------------------------
+static void 
+stupid_openmpi_hack(void)
+{
+  void *handle = NULL;
+  int mode = RTLD_NOW | RTLD_GLOBAL;
+
+  // GNU/Linux and others 
+#ifdef RTLD_NOLOAD
+      mode |= RTLD_NOLOAD;
+#endif
+  if (!handle) handle = dlopen("libmpi.so.20", mode);
+  if (!handle) handle = dlopen("libmpi.so.12", mode);
+  if (!handle) handle = dlopen("libmpi.so.1", mode);
+  if (!handle) handle = dlopen("libmpi.so.0", mode);
+  if (!handle) handle = dlopen("libmpi.so", mode);
+}
+
+#endif
+
 PYBIND11_MODULE(Test, m) {
+#ifdef RHEL_OPENMPI_HACK
+  stupid_openmpi_hack();
+#endif
   py::class_<TestSerialMPI>(m, "SerialMPI")
     .def(py::init<>())
     .def("show", &TestSerialMPI::show)
