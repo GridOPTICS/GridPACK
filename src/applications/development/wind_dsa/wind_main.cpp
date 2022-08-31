06893a9642c685316894529cad6c6d272a2ceeb4
Bruce J Palmer <d3g293@constance03.pnl.gov>
2022-08-31 11:15:00 -0700

    Added some functionality to replace R analysis module used with GOSS.

--- src/applications/development/wind_dsa/wind_main.cpp
+++ src/applications/development/wind_dsa/wind_main.cpp
@@ -16,35 +16,24 @@
 #include "mpi.h"
 #include <ga.h>
 #include <macdecls.h>
-#include "gridpack/math/math.hpp"
+#include "gridpack/include/gridpack.hpp"
 #include "wind_driver.hpp"
 
 // Calling program for the contingency_analysis applications
 
+const char *help = "Dynamic Security Assessment Application for Wind Energy";
+
 int
 main(int argc, char **argv)
 {
-  // Initialize MPI libraries
-  int ierr = MPI_Init(&argc, &argv);
-
-  GA_Initialize();
-  int stack = 200000, heap = 200000;
-  MA_init(C_DBL, stack, heap);
-
-  // Intialize Math libraries
-  gridpack::math::Initialize(&argc, &argv);
+  // Initialize libraries (parallel and math)
+  gridpack::Environment env(argc,argv,help);
 
   {
     gridpack::contingency_analysis::WindDriver driver;
     driver.execute(argc, argv);
   }
 
-  // Terminate Math libraries
-  gridpack::math::Finalize();
-
-  GA_Terminate();
-  // Clean up MPI libraries
-  ierr = MPI_Finalize();
-  return ierr;
+  return 0;
 }
 
