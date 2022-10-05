8dba5b3f89ffa651acba6ed80fa7629413490ad6
William Perkins <william.perkins@pnnl.gov>
2022-10-05 08:48:00 -0700

    Add some options for CMake dependency graph production

---
+++ src/CMakeGraphVizOptions.cmake
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------
+# file: CMakeGraphVizOptions.cmake
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Created October  5, 2022 by Perkins
+# Last Change: 2022-10-05 08:12:10 d3g096
+# -------------------------------------------------------------
+
+set(GRAPHVIZ_EXECUTABLES "OFF")
+set(GRAPHVIZ_EXTERNAL_LIBS "OFF")
+set(GRAPHVIZ_CUSTOM_TARGETS "OFF")
+
