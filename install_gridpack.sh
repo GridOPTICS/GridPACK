37c8dd77b4e0aa3c344b575f45fba2deb9e60c30
Jack Savage <jack.savage@pnnl.gov>
2023-09-05 21:44:01 -0500

    Apply shellcheck suggestions and run formatter

--- install_gridpack.sh
+++ install_gridpack.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # This script installs GridPACK and python wrappter.GridPACK is built in src/build directory and installed in src/install directory.
 
 # This script should be run from the top-level GridPACK directory.
@@ -7,25 +9,22 @@ install_gridpack=true
 install_gridpack_python=true
 
 # Set your python executable here
-python_exe=`which python`
-if test -z ${python_exe}
-then
-    python_exe=`which python3`
+python_exe=$(which python)
+if test -z "${python_exe}"; then
+  python_exe=$(which python3)
 fi
 
-if test -z ${GRIDPACK_ROOT_DIR}
-then
-    export GRIDPACK_ROOT_DIR=${PWD}
-    echo "GRIDPACK_ROOT_DIR = ${GRIDPACK_ROOT_DIR}"
+if test -z "${GRIDPACK_ROOT_DIR}"; then
+  export GRIDPACK_ROOT_DIR=${PWD}
+  echo "GRIDPACK_ROOT_DIR = ${GRIDPACK_ROOT_DIR}"
 fi
 
 # Create directory for installing external packages
-if test -z ${GP_EXT_DEPS}
-then
-     export GP_EXT_DEPS=${GRIDPACK_ROOT_DIR}/external-dependencies
+if test -z "${GP_EXT_DEPS}"; then
+  export GP_EXT_DEPS=${GRIDPACK_ROOT_DIR}/external-dependencies
 fi
 
-cd ${GRIDPACK_ROOT_DIR}
+cd "${GRIDPACK_ROOT_DIR}" || exit
 
 # Set environment variable GRIDPACK_BUILD_DIR and create a build directory
 export GRIDPACK_BUILD_DIR=${GRIDPACK_ROOT_DIR}/src/build
@@ -33,71 +32,69 @@ export GRIDPACK_BUILD_DIR=${GRIDPACK_ROOT_DIR}/src/build
 GRIDPACK_INSTALL_DIR=${GRIDPACK_ROOT_DIR}/src/install
 export GRIDPACK_DIR=${GRIDPACK_INSTALL_DIR}
 
-if ${install_gridpack}
-then
+if ${install_gridpack}; then
 
-  rm -rf $GRIDPACK_BUILD_DIR
-  mkdir $GRIDPACK_BUILD_DIR
+  rm -rf "$GRIDPACK_BUILD_DIR"
+  mkdir "$GRIDPACK_BUILD_DIR"
 
-  rm -rf ${GRIDPACK_INSTALL_DIR}
+  rm -rf "${GRIDPACK_INSTALL_DIR}"
 
-  cd ${GRIDPACK_BUILD_DIR}
-    
+  cd "${GRIDPACK_BUILD_DIR}" || exit
+
   ## GridPACK installation
   echo "Building GridPACK"
 
-#  git checkout develop
+  #  git checkout develop
 
   rm -rf CMake*
 
   cmake_args="-D GA_DIR:STRING=${GP_EXT_DEPS}/ga-5.8/install_for_gridpack \
-   -D BOOST_ROOT:STRING=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack \    
+   -D BOOST_ROOT:STRING=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack \
    -D Boost_DIR:STRING=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack/lib/cmake/Boost-1.78.0 \
-   -D Boost_LIBRARIES:STRING=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack/lib \   
+   -D Boost_LIBRARIES:STRING=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack/lib \
    -D Boost_INCLUDE_DIRS:STRING=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack/include \
-   -D PETSC_DIR:PATH=${GP_EXT_DEPS}/petsc/install_for_gridpack \                  
-   -D MPI_CXX_COMPILER:STRING='mpicxx' \     
-   -D MPI_C_COMPILER:STRING='mpicc' \                                         
-   -D MPIEXEC:STRING='mpiexec' \                                                            
-   -D GRIDPACK_TEST_TIMEOUT:STRING=30 \                                                     
-   -D CMAKE_INSTALL_PREFIX:PATH=${GRIDPACK_INSTALL_DIR} \             
+   -D PETSC_DIR:PATH=${GP_EXT_DEPS}/petsc/install_for_gridpack \
+   -D MPI_CXX_COMPILER:STRING='mpicxx' \
+   -D MPI_C_COMPILER:STRING='mpicc' \
+   -D MPIEXEC:STRING='mpiexec' \
+   -D GRIDPACK_TEST_TIMEOUT:STRING=30 \
+   -D CMAKE_INSTALL_PREFIX:PATH=${GRIDPACK_INSTALL_DIR} \
    -D CMAKE_BUILD_TYPE:STRING=Debug \
    -D BUILD_SHARED_LIBS=YES \
    -D Boost_NO_SYSTEM_PATHS:BOOL=TRUE \
     ..   "
- 
-  cmake ${cmake_args}                                                                                     
 
-   echo "Installing GridPACK develop branch"
-   make -j 10 install
+  cmake "${cmake_args}"
+
+  echo "Installing GridPACK develop branch"
+  make -j 10 install
 fi
 
-if ${install_gridpack_python}
-then
-    echo "Installing GridPACK python wrapper"
-    
-    cd ${GRIDPACK_ROOT_DIR}
-    
-    git submodule update --init
+if ${install_gridpack_python}; then
+  echo "Installing GridPACK python wrapper"
 
-    echo ${GRIDPACK_DIR}
-    cd python
+  cd "${GRIDPACK_ROOT_DIR}" || exit
 
-    export RHEL_OPENMPI_HACK=yes
+  git submodule update --init
 
-    rm -rf build
-    
-    ${python_exe} setup.py build
+  echo "${GRIDPACK_DIR}"
+  cd python || exit
 
-    rm -rf ${GRIDPACK_INSTALL_DIR}/lib/python
-    mkdir ${GRIDPACK_INSTALL_DIR}/lib/python
-    
-    PYTHONPATH="${GRIDPACK_DIR}/lib/python:${PYTHONPATH}"
-    export PYTHONPATH
-    ${python_exe} setup.py install --home="$GRIDPACK_DIR"
-    
+  export RHEL_OPENMPI_HACK=yes
+
+  rm -rf build
+
+  ${python_exe} setup.py build
+
+  rm -rf "${GRIDPACK_INSTALL_DIR}"/lib/python
+  mkdir "${GRIDPACK_INSTALL_DIR}"/lib/python
+
+  PYTHONPATH="${GRIDPACK_DIR}/lib/python:${PYTHONPATH}"
+  export PYTHONPATH
+  ${python_exe} setup.py install --home="$GRIDPACK_DIR"
+
 fi
 
-cd ${GRIDPACK_ROOT_DIR}
+cd "${GRIDPACK_ROOT_DIR}" || exit
 
 echo "Completed GridPACK installation"
--- install_gridpack_deps.sh
+++ install_gridpack_deps.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # This script installs all GridPACK dependencies.The dependencies are installed in external-dependencies directory.
 
 # This script should be run from the top-level GridPACK directory.
@@ -7,39 +9,35 @@ install_boost=true
 install_ga=true
 install_petsc=true
 
-echo $(date)
+date
 
-if test -z ${GRIDPACK_ROOT_DIR}
-then
-    export GRIDPACK_ROOT_DIR=${PWD}
-    echo "GRIDPACK_ROOT_DIR = ${GRIDPACK_ROOT_DIR}"
+if test -z "${GRIDPACK_ROOT_DIR}"; then
+  export GRIDPACK_ROOT_DIR=${PWD}
+  echo "GRIDPACK_ROOT_DIR = ${GRIDPACK_ROOT_DIR}"
 fi
 
-cd ${GRIDPACK_ROOT_DIR}
+cd "${GRIDPACK_ROOT_DIR}" || exit
 
 # Create directory for installing external packages
-if test -z ${GP_EXT_DEPS}
-then
-     export GP_EXT_DEPS=${GRIDPACK_ROOT_DIR}/external-dependencies
-     rm -rf ${GP_EXT_DEPS}
-     mkdir ${GP_EXT_DEPS}
-     echo "GRIDPACK_EXT_DEPENDENCIES_DIR=${GP_EXT_DEPS}"
+if test -z "${GP_EXT_DEPS}"; then
+  export GP_EXT_DEPS=${GRIDPACK_ROOT_DIR}/external-dependencies
+  rm -rf "${GP_EXT_DEPS}"
+  mkdir "${GP_EXT_DEPS}"
+  echo "GRIDPACK_EXT_DEPENDENCIES_DIR=${GP_EXT_DEPS}"
 else
-    if test -d ${GP_EXT_DEPS}
-    then
-	echo "GRIDPACK_EXT_DEPENDENCIES_DIR=${GP_EXT_DEPS}"
-    else
-	mkdir ${GP_EXT_DEPS}
-    fi		
+  if test -d "${GP_EXT_DEPS}"; then
+    echo "GRIDPACK_EXT_DEPENDENCIES_DIR=${GP_EXT_DEPS}"
+  else
+    mkdir "${GP_EXT_DEPS}"
+  fi
 fi
 
-cd ${GP_EXT_DEPS}
+cd "${GP_EXT_DEPS}" || exit
 
-if ${install_boost}
-then
+if ${install_boost}; then
 
   rm -rf boost*
-    
+
   # Download and install Boost
   echo "Downloading Boost-1.78.0"
 
@@ -49,14 +47,14 @@ then
   # Untar
   tar -xf boost_1_78_0.tar.gz
 
-  cd boost_1_78_0
+  cd boost_1_78_0 || exit
 
   # Install boost
   echo "Building Boost-1.78.0"
 
   ./bootstrap.sh --prefix=install_for_gridpack --with-libraries=mpi,serialization,random,filesystem,system
 
-  echo 'using mpi ;' >> project-config.jam
+  echo 'using mpi ;' >>project-config.jam
 
   ./b2 -a -d+2 link=shared stage
 
@@ -66,10 +64,9 @@ then
   echo "Building and Installing Boost libraries complete"
 fi
 
-if ${install_ga}
-then
+if ${install_ga}; then
   # Download, build, and install GA
-  cd ${GP_EXT_DEPS}
+  cd "${GP_EXT_DEPS}" || exit
 
   echo "Downloading GA-5.8"
 
@@ -77,31 +74,30 @@ then
 
   tar -xf ga-5.8.tar.gz
 
-  cd ga-5.8
+  cd ga-5.8 || exit
 
   # Build GA
   echo "Building GA-5.8"
-  ./configure --with-mpi-ts --disable-f77 --without-blas --enable-cxx --enable-i4 --prefix=${PWD}/install_for_gridpack --enable-shared
-  
+  ./configure --with-mpi-ts --disable-f77 --without-blas --enable-cxx --enable-i4 --prefix="${PWD}"/install_for_gridpack --enable-shared
+
   # Install GA
   echo "Installing GA-5.8"
   make -j 10 install
-  
+
   echo "GA-5.8 installation complete"
 fi
 
-if ${install_petsc}
-then
-    
+if ${install_petsc}; then
+
   # Install PETSc 3.16.4
-  cd ${GP_EXT_DEPS}
+  cd "${GP_EXT_DEPS}" || exit
 
   # Download
   echo "Downloading PETSc 3.16.4"
 
   git clone https://gitlab.com/petsc/petsc.git
-    
-  cd petsc
+
+  cd petsc || exit
 
   git checkout tags/v3.16.4 -b v3.16.4
 
@@ -110,24 +106,24 @@ then
 
   # Install PETSc
   echo "Installing PETSc 3.16.4"
-    
-  ./configure --download-superlu_dist --download-metis --download-parmetis --download-suitesparse --download-f2cblaslapack --download-cmake --prefix=${PWD}/install_for_gridpack --scalar-type=complex --with-shared-libraries=1 --download-f2cblaslapack=1
+
+  ./configure --download-superlu_dist --download-metis --download-parmetis --download-suitesparse --download-f2cblaslapack --download-cmake --prefix="${PWD}"/install_for_gridpack --scalar-type=complex --with-shared-libraries=1 --download-f2cblaslapack=1
 
   # Build PETSc
   echo "Building PETSc 3.16.4"
 
-  make 
+  make
 
   # Install PETSc
   echo "Installing PETSc 3.16.4"
 
   make install
-  make check  
-fi  
+  make check
+fi
 
 # update LD_LIBRARY_PATH so that boost,ga, and petsc are on it
 export LD_LIBRARY_PATH=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack/lib:${GP_EXT_DEPS}/ga-5.8/install_for_gridpack/lib:${GP_EXT_DEPS}/petsc/install_for_gridpack/lib:${LD_LIBRARY_PATH}
 
-cd ${GRIDPACK_ROOT_DIR}
+cd "${GRIDPACK_ROOT_DIR}" || exit
 
 echo "Completed installing GridPACK dependencies in ${GP_EXT_DEPS}"
