649da27ec621b8480ca1691ee5c5dae065245745
William Perkins <william.perkins@pnnl.gov>
2023-11-29 13:20:55 -0800

    Initial shot at a Python EMT interface (does not build)

--- src/example_configuration.sh
+++ src/example_configuration.sh
@@ -242,6 +242,7 @@ elif [ $host == "tlaloc" ]; then
         prefix="$GRIDPACK_DIR"
     fi
     
+    prefix="$HOME/Projects/GridPACK-Wind/gridpack-install"
     cmake -Wdev --debug-trycompile \
         --graphviz=GridPACK.dot \
           -D PETSC_DIR:STRING="/home/d3g096/Projects/GridPakLDRD/petsc-3.19.4" \
