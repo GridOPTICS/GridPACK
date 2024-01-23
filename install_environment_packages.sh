240306afeeb54cd62fe959063359e3c48332206b
Jack Savage <jack.savage@pnnl.gov>
2024-01-23 13:50:43 -0600

    Add epel repo and enable crb repo
    For python3-mpi4py-openmpi and its deps

--- install_environment_packages.sh
+++ install_environment_packages.sh
@@ -19,8 +19,11 @@ debian | ubuntu)
   ;;
 fedora | rhel | centos | rocky)
   dnf upgrade --assumeyes --verbose &&
+    dnf install epel-release --assumeyes &&
+    crb enable &&
     dnf install --assumeyes \
-      wget @development git python3.11 python3-pip openmpi-devel cmake pkgconf python3-mpi4py-openmpi
+      wget @development git python3.11 python3-pip openmpi-devel cmake pkgconf \
+      python3-mpi4py-openmpi
   ;;
 *)
   echo "$distribution not supported"
