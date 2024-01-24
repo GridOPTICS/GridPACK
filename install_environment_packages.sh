c580ba2971134aad5c78570ea818052d0e31b49d
Jack Savage <jack.savage@pnnl.gov>
2024-01-23 19:15:02 -0600

    Formatting changes

--- install_environment_packages.sh
+++ install_environment_packages.sh
@@ -12,18 +12,29 @@ distribution=$(
 
 case $distribution in
 debian | ubuntu)
-  apt update &&
-    apt upgrade -y &&
-    apt install -y \
-      wget build-essential git python3.11 python3-pip libopenmpi-dev cmake pkg-config python3-mpi4py
+
+  apt-get update
+  apt-get upgrade --yes
+
+  # install required packages
+  apt-get install --yes \
+    wget build-essential git python3.11 python3-pip libopenmpi-dev cmake pkg-config python3-mpi4py
   ;;
+
 fedora | rhel | centos | rocky)
-  dnf upgrade --assumeyes --verbose &&
-    dnf install epel-release --assumeyes &&
-    crb enable &&
-    dnf install --assumeyes \
-      wget @development git python3.11 python3-pip openmpi-devel cmake pkgconf \
-      python3-mpi4py-openmpi
+
+  dnf upgrade --assumeyes --verbose
+
+  # use epel and crb repos
+  # https://wiki.rockylinux.org/rocky/repo/#notes-on-epel
+  dnf install epel-release --assumeyes
+  crb enable
+
+  # install required packages
+  dnf install --assumeyes \
+    wget @development git python3.11 python3-pip openmpi-devel cmake pkgconf \
+    python3-mpi4py-openmpi
+
   ;;
 *)
   echo "$distribution not supported"
