4a645f7d2b96e0ad519d05afb9e04135936a8f19
Jack Savage <jack.savage@pnnl.gov>
2024-02-05 23:08:45 -0600

    Add back script to check if container tag exists

---
+++ build/check_container_tag_exists.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# creates a file named container-tag-not-found if a tag does not exist in the container registry
+# needs env vars CI_JOB_TOKEN, CI_API_V4_URL, CI_PROJECT_ID, IMAGE_TAG, CI_PROJECT_PATH_SLUG
+# needs curl and jq
+
+# bash options:
+# - errexit: exit on error
+# - nounset: treat unset variables as errors
+# - pipefail: treat whole pipeline as errored if any commands within error
+# https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+set -o errexit -o nounset -o pipefail
+
+check_installed() {
+  if ! command -v "$1" &>/dev/null; then
+    echo "please install '$1'". >&2
+    exit 1
+  fi
+}
+
+check_installed jq
+check_installed curl
+
+# submit a GET request to some endpoint on the project api
+function get_container_registry_repos {
+  # get endpoint or use empty string if not provided
+  # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+  local endpoint=${1:-}
+
+  # curl options:
+  # - no-progress-meter: don't show progress bar but show errors
+  # - location: follow redirects
+  # - fail: return 22 on server errors
+  # - header: send private token for authentication
+  curl \
+    --no-progress-meter \
+    --fail \
+    --location \
+    --header "PRIVATE-TOKEN: ${CI_JOB_TOKEN:?}" \
+    "${CI_API_V4_URL:?}/projects/${CI_PROJECT_ID:?}/registry/repositories${endpoint}"
+}
+
+# query the registry repo id
+repo_id=$(
+  # jq query:
+  # - raw-output: output raw strings instead of json
+  # - exit-status: return 1 if no results
+  # - for each elements of the array
+  # - where the registry repo path is the one we care about
+  # - select the id prop
+  # https://jqlang.github.io/jq/manual/
+  get_container_registry_repos | jq --raw-output --exit-status ".[] | select(.path == \"${CI_PROJECT_PATH_SLUG}\") | .id"
+)
+
+# using that repo id check for a particular tag
+# if command errors, create file named container-tag-not-found
+get_container_registry_repos "/${repo_id}/tags/${IMAGE_TAG:?}" > /dev/null ||
+  touch container-tag-not-found
