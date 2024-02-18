66c080ed345f6dd43410977b70461cdfc19747bf
Jack Savage <jack.savage@pnnl.gov>
2024-02-18 13:25:25 -0600

    Load mpi module from the boost script directly
    Remove redundancy from dockerfile

--- build/install_boost.sh
+++ build/install_boost.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+# get the parent directory of this script
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# load mpi module for RHEL
+source "$script_dir/install_package_deps_lib.sh"
+load_mpi_module
+
 boost_version="${BOOST_VERSION:-1.78.0}"
 
 echo "--- Installing Boost ${boost_version} ---"
