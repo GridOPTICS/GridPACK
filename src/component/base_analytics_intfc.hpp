7d1e3aa4b142ca4c9eaa98c93dc15695403e4c8e
Bruce J Palmer <d3g293@constance03.pnl.gov>
2024-03-19 10:20:05 -0700

    Modified routines to get number of generators, loads, etc. so that they only return the number of objects with status 'ON' and modified partition function to push a pointer to the data collection object into the component data analytics interface.

--- src/component/base_analytics_intfc.hpp
+++ src/component/base_analytics_intfc.hpp
@@ -58,11 +58,6 @@ public:
   virtual int numStorage();
   
   /**
-   * return number of storage units on bus
-   */
-  virtual int numStorage();
-  
-  /**
    * return number of loads on bus
    */
   virtual int numLoads();
