e00842bb8d1976edc9c371a1528922cc6c7437ae
Bruce J Palmer <d3g293@constance03.pnl.gov>
2024-03-20 10:32:44 -0700

    Fixed a bug in initializing analytics interface and added calls to powerflow application that report on the total number of generators, loads and lines in the system.

--- src/applications/powerflow/pf_main.cpp
+++ src/applications/powerflow/pf_main.cpp
@@ -80,7 +80,6 @@ int main(int argc, char **argv)
       std::cout<<"Number of loads in network:      "<<nload<<std::endl;
       std::cout<<"Number of lines in network:      "<<nline<<std::endl;
     }
-
     if (useNonLinear) {
       pf_app.nl_solve();
     } else {
