0ca283f375722de1bff0243b4e97fedc76b425cf
William Perkins <william.perkins@pnnl.gov>
2024-05-02 10:19:18 -0700

    Add numLines() to HADREC and Python API

--- python/src/tests/gridpack_test.py
+++ python/src/tests/gridpack_test.py
@@ -105,6 +105,7 @@ class GridPACKTester(TestCase):
         print("Number of branches: %d" % (hadapp.totalBranches()))
         print("Number of generators: %d" % (hadapp.numGenerators()))
         print("Number of loads: %d" % (hadapp.numLoads()))
+        print("Number of lines: %d" % (hadapp.numLines()))
         print("Number of storage units: %d" % (hadapp.numStorage()))
         print("Branches connected to bus 1: ", hadapp.getConnectedBranches(1))
         print("Buses connected to branch 1: ", hadapp.getBranchEndpoints(1))
