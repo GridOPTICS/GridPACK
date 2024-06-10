b06fbae78932b8fd6f7c46b966f721f3e458f02a
Shri <shri@pnnl.gov>
2024-06-09 21:39:59 -0500

    Updated gridpack installation files and dependenices. Disabled python

--- install_gridpack_deps.sh
+++ install_gridpack_deps.sh
@@ -41,27 +41,27 @@ then
   rm -rf boost*
     
   # Download and install Boost
-  echo "Downloading Boost-1.78.0"
+  echo "Downloading Boost-1.81.0"
 
   # Download Boost
-  wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
+  wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
 
   # Untar
-  tar -xf boost_1_78_0.tar.gz
+  tar -xf boost_1_81_0.tar.gz
 
-  cd boost_1_78_0
+  cd boost_1_81_0
 
   # Install boost
-  echo "Building Boost-1.78.0"
+  echo "Building Boost-1.81.0"
 
   ./bootstrap.sh --prefix=install_for_gridpack --with-libraries=mpi,serialization,random,filesystem,system
 
   echo 'using mpi ;' >> project-config.jam
 
-  ./b2 -a -d+2 link=shared stage
+  ./b2 -a -d+2 link=static stage
 
   echo "Installing Boost-1.78.0"
-  ./b2 -a -d+2 link=shared install
+  ./b2 -a -d+2 link=static install
 
   echo "Building and Installing Boost libraries complete"
 fi
@@ -81,7 +81,7 @@ then
 
   # Build GA
   echo "Building GA-5.8"
-  ./configure --with-mpi-ts --disable-f77 --without-blas --enable-cxx --enable-i4 --prefix=${PWD}/install_for_gridpack --enable-shared
+  ./configure --with-mpi-ts --disable-f77 --without-blas --enable-cxx --enable-i4 --prefix=${PWD}/install_for_gridpack
   
   # Install GA
   echo "Installing GA-5.8"
@@ -93,40 +93,40 @@ fi
 if ${install_petsc}
 then
     
-  # Install PETSc 3.16.4
+  # Install PETSc 3.20.1
   cd ${GP_EXT_DEPS}
 
   # Download
-  echo "Downloading PETSc 3.16.4"
+  echo "Downloading PETSc"
 
   git clone https://gitlab.com/petsc/petsc.git
     
   cd petsc
 
-  git checkout tags/v3.16.4 -b v3.16.4
+  git checkout tags/v3.20.1 -b v3.20.1
 
   export PETSC_DIR=${PWD}
   export PETSC_ARCH=build-dir
 
   # Install PETSc
-  echo "Installing PETSc 3.16.4"
+  echo "Installing PETSc 3.20.1"
     
-  ./configure --download-superlu_dist --download-metis --download-parmetis --download-suitesparse --download-f2cblaslapack --download-cmake --prefix=${PWD}/install_for_gridpack --scalar-type=complex --with-shared-libraries=1 --download-f2cblaslapack=1
+  ./configure --download-superlu_dist --download-metis --download-parmetis --with-debugging=0 --prefix=${PWD}/install_for_gridpack
 
   # Build PETSc
-  echo "Building PETSc 3.16.4"
+  echo "Building PETSc 3.20.1"
 
   make 
 
   # Install PETSc
-  echo "Installing PETSc 3.16.4"
+  echo "Installing PETSc 3.20.1"
 
   make install
   make check  
 fi  
 
 # update LD_LIBRARY_PATH so that boost,ga, and petsc are on it
-export LD_LIBRARY_PATH=${GP_EXT_DEPS}/boost_1_78_0/install_for_gridpack/lib:${GP_EXT_DEPS}/ga-5.8/install_for_gridpack/lib:${GP_EXT_DEPS}/petsc/install_for_gridpack/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${GP_EXT_DEPS}/boost_1_81_0/install_for_gridpack/lib:${GP_EXT_DEPS}/ga-5.8/install_for_gridpack/lib:${GP_EXT_DEPS}/petsc/install_for_gridpack/lib:${LD_LIBRARY_PATH}
 
 cd ${GRIDPACK_ROOT_DIR}
 
