375948cbb76dad9219158456866997d3fbb10216
Jain, Milan <milan.jain@pnnl.gov>
2024-11-15 12:04:59 -0800

    time sync with Grid2Op

--- python/src/example/test_grid2op/config.py
+++ python/src/example/test_grid2op/config.py
@@ -1,7 +1,7 @@
 from grid2op.Action import TopologyAction
 from grid2op.Reward import L2RPNReward
 from grid2op.Rules import DefaultRules
-from grid2op.Chronics import Multifolder
+from grid2op.Chronics import ChangeNothing
 from grid2op.Chronics import GridStateFromFileWithForecasts
 
 import sys
