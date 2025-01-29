c8628bf8843aa88abd93dab5b5a394c069d9278b
Jain, Milan <milan.jain@pnnl.gov>
2025-01-29 09:49:01 -0800

    minor fixes for integration

--- python/src/tests/gridpack_test.py
+++ python/src/tests/gridpack_test.py
@@ -101,18 +101,6 @@ class GridPACKTester(TestCase):
         print("Branches connected to bus 1: ", hadapp.getConnectedBranches(1))
         print("Buses connected to branch 1: ", hadapp.getBranchEndpoints(1))
 
-<<<<<<< HEAD
-        print("Number of buses:  %d" % (hadapp.totalBuses()))
-        print("Number of branches: %d" % (hadapp.totalBranches()))
-        print("Number of generators: %d" % (hadapp.numGenerators()))
-        print("Number of loads: %d" % (hadapp.numLoads()))
-        print("Number of lines: %d" % (hadapp.numLines()))
-        print("Number of storage units: %d" % (hadapp.numStorage()))
-        print("Branches connected to bus 1: ", hadapp.getConnectedBranches(1))
-        print("Buses connected to branch 1: ", hadapp.getBranchEndpoints(1))
-
-=======
->>>>>>> develop
         busfaultlist = gridpack.dynamic_simulation.EventVector()
 
     #     arg = "input_tamu500_step005.xml"
