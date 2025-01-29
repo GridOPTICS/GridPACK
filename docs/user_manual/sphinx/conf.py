eeeee6638b9f2307a05cd4d3dd61d1e44fe60431
bruce.palmer@pnnl.gov <d3g293@we48161.pnl.gov>
2025-01-29 10:41:07 -0800

    Modified conf.py to add a logo to documentation.

--- docs/user_manual/sphinx/conf.py
+++ docs/user_manual/sphinx/conf.py
@@ -96,6 +96,12 @@ numfig_format = {
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = 'logo.svg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}`
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
