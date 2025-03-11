df15c5d3cc45bb76721cce68c6a545b7f131cce2
William Perkins <william.perkins@pnnl.gov>
2025-03-11 07:00:21 -0700

    Changes needed to make Python work on Mac

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,6 @@
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # Created February 17, 2020 by Perkins
-# Last Change: 2025-02-11 10:50:33 d3g096
 # -------------------------------------------------------------
 
 import os
@@ -39,7 +38,7 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable,
+                      '-DPython_EXECUTABLE=' + sys.executable,
                       '-DCMAKE_VERBOSE_MAKEFILE=YES' ]
 
         cfg = 'Debug' if self.debug else 'Release'
