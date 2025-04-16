af876ef88222e48d1b88f29f8889aa5752269862
Jain, Milan <milan.jain@pnnl.gov>
2025-04-16 07:57:05 -0700

    fixed issues for gridpack reset

--- python/src/grid2op_backend.py
+++ python/src/grid2op_backend.py
@@ -1,4 +1,5 @@
 import os, sys
+import warnings
 import numpy as np
 import pandas as pd
 from typing import Optional, Tuple, Union
@@ -286,6 +287,7 @@ class GridPACKBackend(Backend):
         # called once   
         This step is called only ONCE, when the grid2op environment is created. In this step, you read a grid file (in the format that you want) and the backend should inform grid2op about the "objects" on this powergrid and their location.
         '''
+        # breakpoint()
         # first load the grid from the file
         # self.full_path = path
         # if filename is not None:
@@ -295,7 +297,7 @@ class GridPACKBackend(Backend):
         # read XML to get timestep
         with open(self.full_path, 'r') as f:
             data = f.read()
-        bs_data = BeautifulSoup(data, "lxml")
+        bs_data = BeautifulSoup(data, features="lxml")
         # timestep
         self._gridpack_stepsize = float(bs_data.find('timestep').text)
         print(f"[INFO] GridPACK simulation step size: {self._gridpack_stepsize} seconds")
@@ -406,9 +408,11 @@ class GridPACKBackend(Backend):
               grid_filename: Optional[Union[os.PathLike, str]]=None
             ) -> None:
         # TODO: Reset GridPACK simulator
-        # self._counter = 0
-        # self._counter_time = self._counter * self._gridpack_stepsize
-        pass
+        # breakpoint()
+        self._del_gridpack()
+        self._init_gridpack()
+        self.load_grid(path, grid_filename)
+        # pass
 
     def apply_action(self, backendAction: Union["grid2op.Action._backendAction._BackendAction", None]) -> None:
         '''
@@ -592,6 +596,7 @@ class GridPACKBackend(Backend):
         self._reset_data_collectors()
 
         # run GridPACK
+        # breakpoint()
         for i in range(n_steps):
             # run GridPACK simulation by one time step
             self._dsapp.executeOneSimuStep()  
@@ -758,7 +763,19 @@ class GridPACKBackend(Backend):
         v_ex[[~s for s in status]] = 0.
         
         return p_ex, q_ex, v_ex, a_ex
-    
+
+    def copy(self):
+        # copy the gridpack object
+        res = type(self)(**self._gridpack_kwargs)
+
+        # copy from base class (backend)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", FutureWarning)
+            # warnings depending on pandas version and pp version
+            res._grid = copy.deepcopy(self._grid)
+        
+        return res
+
     def close(self):
         self._dsapp = None
         self.env = None
