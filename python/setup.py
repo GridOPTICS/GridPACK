6e76260568a567471894d09dad3f65f69860ce9f
Yousu Chen <yousu.chen@pnnl.gov>
2026-07-18 06:47:38 -0700

    Populate pyproject.toml with PEP 621 metadata; slim setup.py to CMake shim

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,11 @@
 # -------------------------------------------------------------
 # file: setup.py
 # -------------------------------------------------------------
-# -------------------------------------------------------------
-# -------------------------------------------------------------
-# -------------------------------------------------------------
-# Created February 17, 2020 by Perkins
-# Added console_scripts entry point
-# March, 2026 by Yousu Chen
+# Package metadata lives in pyproject.toml.  This file exists only
+# to drive the CMake build of the pybind11 extension
+# `gridpack._gridpack`; the rest of the packaging (name, version,
+# dependencies, entry points, ...) comes from pyproject.toml under
+# the setuptools build backend.
 # -------------------------------------------------------------
 
 import os
@@ -26,19 +25,23 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            subprocess.check_output(['cmake', '--version'])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
 
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPython_EXECUTABLE=' + sys.executable,
-                      '-DCMAKE_VERBOSE_MAKEFILE=YES' ]
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+            '-DPython_EXECUTABLE=' + sys.executable,
+            '-DCMAKE_VERBOSE_MAKEFILE=YES',
+        ]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
@@ -47,28 +50,26 @@ class CMakeBuild(build_ext):
         build_args += ['--', '-j2']
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        print("PRINTING:", ext.sourcedir)
-        # sys.exit(1)
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''),
+            self.distribution.get_version(),
+        )
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(
+            ['cmake', ext.sourcedir] + cmake_args,
+            cwd=self.build_temp,
+            env=env,
+        )
+        subprocess.check_call(
+            ['cmake', '--build', '.'] + build_args,
+            cwd=self.build_temp,
+        )
+
 
 setup(
-    name='gridpack',
-    version='0.0.1',
-    author='W.A. Perkins',
-    author_email='william.perkins@pnnl.gov',
-    description='A Python interface to some GridPACK application modules',
-    long_description='',
-    packages=['gridpack', 'gridpack.cli'],
-    package_dir={'gridpack': 'gridpack', 'gridpack.cli': 'gridpack/cli'},
     ext_modules=[CMakeExtension('gridpack._gridpack')],
     cmdclass=dict(build_ext=CMakeBuild),
-    install_requires=["mpi4py"],
-    zip_safe=False,
     scripts=[
         'src/hello.py',
         'src/hello_comm.py',
@@ -81,11 +82,4 @@ setup(
         'src/pf.py',
         'src/stes.py',
     ],
-    entry_points={
-        'console_scripts': [
-            'gridpack=gridpack.cli.main:main',
-        ],
-    },
-    test_suite='nose.collector',
-    tests_require=['nose'],
 )
